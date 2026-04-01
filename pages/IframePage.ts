a1b536b6b632df8753cf385002e8027ea245772c
TUBIANA Séverine <s.tubiana@Viveris.lan>
2026-04-01 11:54:57 +0200

    scripts 3

---
+++ pages/IframePage.ts
@@ -0,0 +1,24 @@
+// pages/IframePage.ts
+// rajouter FrameLocator, Locator dans import à la ligne 3 si on retire les commentaires
+import { Page } from '@playwright/test';
+
+export class IframePage {
+  readonly page: Page;
+  //readonly editorFrame: FrameLocator;
+  //readonly textArea: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    //this.editorFrame = page.frameLocator('#mce_0_ifr');
+    //this.textArea = this.editorFrame.locator('body#tinymce');
+  }
+
+  async goto() {
+    await this.page.goto('/tinymce');
+  }
+
+  //pb de droit sur le site - modification non possible
+  //async writeInEditor(text: string) {
+   // await this.textArea.fill(text);
+  //}
+}
